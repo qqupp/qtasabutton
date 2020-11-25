b678df20e5a1a9720d242919847ca855c352a359
Paolo Picci <paolo.picci@springernature.com>
2020-11-25 15:57:27 +0000

    small refactoring

--- src/main/scala/queuegarden/sql/HomeMadeMigration.scala
+++ src/main/scala/queuegarden/sql/HomeMadeMigration.scala
@@ -4,19 +4,13 @@ import cats.effect.Bracket
 import doobie._
 import doobie.implicits._
 
-trait Migration[F[_]] {
-  def migrate: F[Unit]
-}
-
 class HomeMadeMigration[F[_]](
-    transactor: Transactor[F]
-  )(implicit B: Bracket[F, Throwable])
-    extends Migration[F] {
+    transactor: Transactor[F]) {
 
   val userTable: doobie.Update0 =
     sql"""create table if not exists user (id integer, name string)""".update
 
-  val migrate: F[Unit] =
+  def migrate(implicit B: Bracket[F, Throwable]): F[Unit] =
     B.flatMap(userTable.run.transact(transactor))(_ => B.pure(()))
 
 }
