6739354ebbff22c206be5c861219ad8dd9c44e5c
Paolo Picci <paolo.picci@springernature.com>
2020-11-25 20:58:19 +0000

    add migration to main

--- src/main/scala/queuegarden/Main.scala
+++ src/main/scala/queuegarden/Main.scala
@@ -1,19 +1,30 @@
 package queuegarden
 
 import cats.effect.{ExitCode, IO, IOApp}
+import doobie.util.transactor
+import doobie.util.transactor.Transactor
 import queuegarden.config.{ConfigLoader, ServerConfig}
+import queuegarden.sql.{DBTransactor, HomeMadeMigration}
+import cats.implicits._
 
 object Main extends IOApp {
 
   val config = ConfigLoader.config
 
-  val server = new Server(config.server)
-
-  def run(args: List[String]): IO[ExitCode] = {
-    server
+  val runServer: IO[Unit] =
+    new Server(config.server)
       .stream[IO]
       .compile
       .drain
+
+  val DBTransactor: Transactor[IO] =
+    new DBTransactor(config.database).xa[IO]
+
+  val runDBMigration: IO[Unit] =
+    new HomeMadeMigration(DBTransactor).migrate
+
+  def run(args: List[String]): IO[ExitCode] = {
+    runDBMigration *> runServer
       .as(ExitCode.Success)
   }
 
